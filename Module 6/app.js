c0499167d8d0b68b5d3d4f70f6bf9d197cdc3476
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-11-11 18:45:13 -0500

    Update Module 6/app.js

---
+++ Module 6/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const configRoutes = require("./routes");
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("Server is running! Routed on http://localhost:3000");
+});
